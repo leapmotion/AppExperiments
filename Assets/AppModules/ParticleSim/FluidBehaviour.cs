cfded30d1cbe86800f980eb4dd2375915de20714
Nicholas Benson <nickjbenson@gmail.com>
2018-07-10 18:06:51 -0700

    Trying out some fluid stuff

---
+++ Assets/AppModules/ParticleSim/FluidBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParticleSim {
+
+  public class FluidBehaviour : MonoBehaviour {
+    
+    public ComputeShader computeShader;
+
+    private void Start() {
+      spawnParticles();
+    }
+
+    private const int NUM_PARTICLES = 16000;
+
+    private void spawnParticles() {
+      
+    }
+
+  }
+
+}
