c30bb7b85cadb6c56ef1e1b7f9e4fd9bcc0f1280
Nick Benson <nickjbenson@gmail.com>
2018-10-15 15:06:26 -0700

    bluh bluh local changes?

---
+++ Assets/AppModules/ParticleSim/FluidSimulation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ParticleSim {
+
+	public class FluidSimulation : ScriptableObject {
+
+		public const int MAX_PARTICLES = 65535;
+
+		private int _particleCount = 0;
+		public int particleCount { get { return _particleCount; } }
+
+	}
+
+}
